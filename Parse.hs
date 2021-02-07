f3d22f8ba8c299e005b068e764c29e3ada4e15fb
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-02-06 23:41:50 -0500

    separate parser output from errors

--- Parse.hs
+++ Parse.hs
@@ -4,60 +4,61 @@ import Scan
 import Ast
 import Diagnostic
 
-parse :: [Located Token] -> Located Expr
+data ParseError = UnexpectedEOF
+                | ExpectedExpr
+    deriving (Show)
+
+parse :: [Located Token] -> (Maybe (Located Expr), [ParseError])
 parse = parseExpr
 
-parseExpr :: [Located Token] -> Located Expr
-parseExpr [] = error "unexpected eof while parsing (this error should eventually be replaced Left ParseError)"
-parseExpr tokens =
-    let (Located { value = firstToken }:_) = tokens
+parseExpr :: [Located Token] -> (Maybe (Located Expr), [ParseError])
+parseExpr [] = (Nothing, [UnexpectedEOF])
+parseExpr (locatedFirst:tokens) =
+    let Located { value = firstToken } = locatedFirst
     in case firstToken of
-        BoolLiteral _ -> parseBoolExpr tokens
-        NumberLiteral _ -> parseNumberExpr tokens
-        StringLiteral _ -> parseStringExpr tokens
-        OpenParen -> parseGroupingExpr tokens
-        Minus -> parseUnaryExpr tokens
-        Bang -> parseUnaryExpr tokens
-        _ -> error "expected expr"
+        BoolLiteral _   -> parseBoolExpr locatedFirst tokens
+        NumberLiteral _ -> parseNumberExpr locatedFirst tokens
+        StringLiteral _ -> parseStringExpr locatedFirst tokens
+        OpenParen       -> parseGroupingExpr locatedFirst tokens
+        Minus           -> parseUnaryExpr locatedFirst tokens
+        Bang            -> parseUnaryExpr locatedFirst tokens
+        _               -> (Nothing, [ExpectedExpr])
 
-parseBoolExpr :: [Located Token] -> Located Expr
-parseBoolExpr [] = error "parse a bool expr with empty token stream that was not filtered out by parseExpr"
-parseBoolExpr (locatedToken:_) =
+parseBoolExpr :: Located Token -> [Located Token] -> (Maybe (Located Expr), [ParseError])
+parseBoolExpr locatedToken _ =
     let boolToken = value locatedToken
     in case boolToken of
-        BoolLiteral bool -> copyLocation (BoolExpr $ copyLocation bool locatedToken) locatedToken
+        BoolLiteral bool -> (Just $ copyLocation (BoolExpr $ copyLocation bool locatedToken) locatedToken, [])
         _ -> error "parse a bool expr where the first token is not a BoolLiteral"
 
-parseNumberExpr :: [Located Token] -> Located Expr
-parseNumberExpr [] = error "parse a number expr with empty token stream that was not filtered out by parseExpr"
-parseNumberExpr (locatedToken:_) =
+parseNumberExpr :: Located Token -> [Located Token] -> (Maybe (Located Expr), [ParseError])
+parseNumberExpr locatedToken _ =
     let numToken = value locatedToken
     in case numToken of
-        NumberLiteral num -> copyLocation (NumberExpr $ copyLocation num locatedToken) locatedToken
+        NumberLiteral num -> (Just $ copyLocation (NumberExpr $ copyLocation num locatedToken) locatedToken, [])
         _ -> error "parse a num expr where the first token is not a NumberLiteral"
 
-parseStringExpr :: [Located Token] -> Located Expr
-parseStringExpr [] = error "parse a string expr with empty token stream that was not filtered out by parseExpr"
-parseStringExpr (locatedToken:_) =
+parseStringExpr :: Located Token -> [Located Token] -> (Maybe (Located Expr), [ParseError])
+parseStringExpr locatedToken _ =
     let strToken = value locatedToken
     in case strToken of
-        StringLiteral str -> copyLocation (StringExpr $ copyLocation str locatedToken) locatedToken
+        StringLiteral str -> (Just $ copyLocation (StringExpr $ copyLocation str locatedToken) locatedToken, [])
         _ -> error "parse a str expr where the first token is not a StringLiteral"
 
-parseGroupingExpr :: [Located Token] -> Located Expr
-parseGroupingExpr [] = error "parse a grouping expr with empty token stream that was not filtered out by parseExpr"
-parseGroupingExpr (_:nextTokens) =
-    let expr = parseExpr nextTokens
-    in copyLocation (GroupingExpr expr) expr
+parseGroupingExpr :: Located Token -> [Located Token] -> (Maybe (Located Expr), [ParseError])
+parseGroupingExpr _ nextTokens =
+    let (maybeExpr, errs) = parseExpr nextTokens
+        grouped = (\expr -> copyLocation (GroupingExpr expr) expr) <$> maybeExpr
+    in (grouped, errs)
 
-parseUnaryExpr :: [Located Token] -> Located Expr
-parseUnaryExpr [] = error "parse a unary expr with empty token stream that was not filtered out by parseExpr"
-parseUnaryExpr (locOp:nextTokens) =
+parseUnaryExpr :: Located Token -> [Located Token] -> (Maybe (Located Expr), [ParseError])
+parseUnaryExpr locOp nextTokens =
     let opTok = value locOp
         operator = case opTok of
             Bang -> Not
             Minus -> Neg
             _ -> error $ "parse a unary expr with invalid unary operator: " ++ show opTok
-        operand = parseExpr nextTokens
-    in unionLocation locOp operand $ UnaryExpr (copyLocation operator locOp) operand
+        (operand, operandErrors) = parseExpr nextTokens
+        unaryExpr = (\unexpr -> unionLocation locOp unexpr (UnaryExpr (copyLocation operator locOp) unexpr)) <$> operand
+    in (unaryExpr, operandErrors)
 
