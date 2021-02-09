8f7a90868be15b448dd3ee538d2c2bb53cdf3a52
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-02-09 17:55:22 -0500

    support runtime errors correctly

--- src/Runtime/Value.hs
+++ src/Runtime/Value.hs
@@ -6,3 +6,14 @@ data LoxValue = LoxNil
               | LoxString String
               deriving (Show, Eq)
 
+stringifyType :: LoxValue -> String
+stringifyType LoxNil = "nil"
+stringifyType (LoxBool _) = "bool"
+stringifyType (LoxNumber _) = "number"
+stringifyType (LoxString _) = "string"
+
+stringifyValue :: LoxValue -> String
+stringifyValue LoxNil = "nil"
+stringifyValue (LoxBool b) = if b then "true" else "false"
+stringifyValue (LoxNumber n) = show n
+stringifyValue (LoxString s) = s
--- src/Treewalk/Interpret.hs
+++ src/Treewalk/Interpret.hs
@@ -3,51 +3,72 @@ module Treewalk.Interpret (interpret) where
 import Frontend.Ast
 import Frontend.Diagnostic
 import Runtime.Value
+import Runtime.Error
 
-interpret :: Located Expr -> LoxValue
-interpret (Located _ (BinaryExpr lhs (Located operatorSpan operator) rhs)) =
-    let lhsEval = interpret lhs
-        rhsEval = interpret rhs
-        (lhsDouble, rhsDouble) = checkOperandsDouble operatorSpan lhsEval rhsEval
-
+interpret :: Located Expr -> Either RuntimeError (Located LoxValue)
+interpret (Located outSpan (BinaryExpr lhs@(Located lhsSpan _) (Located operatorSpan operator) rhs@(Located rhsSpan _))) =
+    interpret lhs >>= \locatedLhsValue@(Located _ lhsValue) ->
+    interpret rhs >>= \locatedRhsValue@(Located _ rhsValue) ->
+    let checkDoubles = checkOperandsDouble operatorSpan locatedLhsValue locatedRhsValue
     in case operator of
-        Sub  -> LoxNumber $ lhsDouble - rhsDouble
-        Mult -> LoxNumber $ lhsDouble * rhsDouble
-        Div  -> LoxNumber $ lhsDouble / rhsDouble
+        Sub ->
+            checkDoubles >>= \(lhsDouble, rhsDouble) ->
+            out $ LoxNumber $ lhsDouble - rhsDouble
+        Mult ->
+            checkDoubles >>= \(lhsDouble, rhsDouble) ->
+            out $ LoxNumber $ lhsDouble * rhsDouble
+        Div ->
+            checkDoubles >>= \(lhsDouble, rhsDouble) ->
+            out $ LoxNumber $ lhsDouble / rhsDouble
+        Add ->
+            case (lhsValue, rhsValue) of
+                (LoxNumber lhsDouble, LoxNumber rhsDouble) -> out $ LoxNumber $ lhsDouble + rhsDouble
+                (LoxString lhsStr, LoxString rhsStr) -> out $ LoxString $ lhsStr ++ rhsStr
 
-        Add -> case (lhsEval, rhsEval) of
-            (LoxNumber lhsDouble, LoxNumber rhsDouble) -> LoxNumber $ lhsDouble + rhsDouble
-            (LoxString lhsStr, LoxString rhsStr) -> LoxString $ lhsStr ++ rhsStr
-            _ -> error "Operands must be two numbers or two strings"
+                _ -> Left $ OperandsMustBeNumbersOrStrings operatorSpan locatedLhsValue locatedRhsValue
 
-        Greater      -> LoxBool $ lhsDouble > rhsDouble
-        GreaterEqual -> LoxBool $ lhsDouble >= rhsDouble
-        Less         -> LoxBool $ lhsDouble < rhsDouble
-        LessEqual    -> LoxBool $ lhsDouble <= rhsDouble
+        Greater      ->
+            checkDoubles >>= \(lhsDouble, rhsDouble) ->
+            out $ LoxBool $ lhsDouble > rhsDouble
+        GreaterEqual ->
+            checkDoubles >>= \(lhsDouble, rhsDouble) ->
+            out $ LoxBool $ lhsDouble >= rhsDouble
+        Less         ->
+            checkDoubles >>= \(lhsDouble, rhsDouble) ->
+            out $ LoxBool $ lhsDouble < rhsDouble
+        LessEqual    ->
+            checkDoubles >>= \(lhsDouble, rhsDouble) ->
+            out $ LoxBool $ lhsDouble <= rhsDouble
 
-        Equal    -> LoxBool $ lhsEval == rhsEval
-        NotEqual -> LoxBool $ not $ lhsEval == rhsEval
+        Equal    -> out $ LoxBool $ lhsValue == rhsValue
+        NotEqual -> out $ LoxBool $ not $ lhsValue == rhsValue
 
-interpret (Located _ (UnaryExpr (Located _ operator) operand)) = 
-    let operandValue = interpret operand
-    in case operator of
+    where
+        out value = Right $ Located outSpan value
+
+interpret (Located outSpan (UnaryExpr (Located operatorSpan operator) operandAST)) =
+    interpret operandAST >>= \loctedOperand@(Located _ operandValue) ->
+    case operator of
         Neg ->
             case operandValue of
-                LoxNumber d -> LoxNumber $ -d
+                LoxNumber d -> Right $ Located outSpan $ LoxNumber $ -d
                 _ -> error "Operand must be a number"
 
-        Not -> LoxBool $ not (isTruthy operandValue)
+        Not -> Right $ Located outSpan $ LoxBool $ not (isTruthy operandValue)
 
-interpret (Located _ (GroupingExpr expr)) = interpret expr
-interpret (Located _ (BoolExpr (Located _ b))) = LoxBool b
-interpret (Located _ (NumberExpr (Located _ n))) = LoxNumber n
-interpret (Located _ (StringExpr (Located _ s))) = LoxString s
+interpret (Located groupingSpan (GroupingExpr expr)) =
+    interpret expr >>= \(Located _ insideValue) ->
+    Right $ Located groupingSpan $ insideValue
+
+interpret (Located span (BoolExpr (Located _ b))) = Right $ Located span $ LoxBool b
+interpret (Located span (NumberExpr (Located _ n))) = Right $ Located span $ LoxNumber n
+interpret (Located span (StringExpr (Located _ s))) = Right $ Located span $ LoxString s
 
 isTruthy :: LoxValue -> Bool
 isTruthy LoxNil = False
 isTruthy (LoxBool b) = b
 isTruthy _ = True
 
-checkOperandsDouble :: Span -> LoxValue -> LoxValue -> (Double, Double)
-checkOperandsDouble _ (LoxNumber lhs) (LoxNumber rhs) = (lhs, rhs)
-checkOperandsDouble _ _ _ = error "Operands must be numbers"
+checkOperandsDouble :: Span -> Located LoxValue -> Located LoxValue -> Either RuntimeError (Double, Double)
+checkOperandsDouble _ (Located _ (LoxNumber lhs)) (Located _ (LoxNumber rhs)) = Right (lhs, rhs)
+checkOperandsDouble operatorSpan lhs rhs = Left $ OperandsMustBeNumbers operatorSpan lhs rhs
