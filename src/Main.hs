8c22ebd0a22b21f24d567aaeaa1afe4c59e2fea2
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-02-09 20:38:46 -0500

    add useless arg parsing

--- src/Main.hs
+++ src/Main.hs
@@ -2,18 +2,88 @@ module Main where
 
 import System.Environment
 import System.IO
+import System.Console.ParseArgs
 import Frontend.Scan
 import Frontend.Parse
 import Frontend.Diagnostic
 import Treewalk.Interpret
 import Runtime.Value
 
+data InterpreterSettings = InterpreterSettings
+                           { errorFormat :: ErrorFormat
+                           , backend :: Backend
+                           , file :: Maybe String
+                           }
+
+data ErrorFormat = DefaultErrorFormat | OriginalErrorFormat
+                 deriving Show
+data Backend = Treewalk | StackBC | RegisterBC
+             deriving Show
+
+readInterpreterSettings :: Args String -> Either String InterpreterSettings
+readInterpreterSettings args =
+    let errorFormatStr = getRequiredArg args "errformat" :: String
+        backendStr = getRequiredArg args "backend" :: String
+        interpretingFile = getArg args "file" :: Maybe String
+
+    in (case errorFormatStr of
+            "default" -> Right DefaultErrorFormat
+            "original" -> Left "original error format is not implemented (yet)"
+            _ -> Left $ "invalid error format: '" ++ errorFormatStr ++ "'"
+        ) >>= \errorFormat ->
+        (case backendStr of
+            "treewalk" -> Right Treewalk
+            "stackbc" -> Left "stack bytecode interpreter is not implemented (yet)"
+            "registerbc" -> Left "register bytecode interpreter is not implemented (yet)"
+            _ -> Left $ "invalid backend: '" ++ backendStr ++ "'"
+        ) >>= \backend ->
+        Right InterpreterSettings
+        { errorFormat = errorFormat
+        , backend = backend
+        , file = interpretingFile
+        }
+
 main :: IO ()
-main = getArgs >>=
-    \args -> case args of
-        []      -> repl
-        file:[] -> runFile file
-        _       -> putStrLn "bad usage"
+main =
+    getProgName >>= \progName ->
+    getArgs >>= \args ->
+    let parsedArgs = parseArgs argsControl argsSpec progName args
+        settings = readInterpreterSettings parsedArgs
+    in case settings of
+        Right set -> runInterpreter set
+        Left err -> hPutStrLn stderr $ "hlox: " ++ err
+    where
+        argsControl = ArgsParseControl
+                     { apcComplete = ArgsComplete
+                     , apcDash = ArgsHardDash
+                     }
+        argsSpec = [ Arg
+                     { argIndex = "errformat"
+                     , argAbbr = Just 'e'
+                     , argName = Just "error-format"
+                     , argData = argDataDefaulted "format" ArgtypeString "default"
+                     , argDesc = "the error format"
+                     }
+                   , Arg
+                     { argIndex = "backend"
+                     , argAbbr = Just 'b'
+                     , argName = Just "backend"
+                     , argData = argDataDefaulted "backend" ArgtypeString "treewalk"
+                     , argDesc = "the backend to interpret with"
+                     }
+                   , Arg
+                     { argIndex = "file"
+                     , argAbbr = Nothing
+                     , argName = Nothing
+                     , argData = argDataOptional "file" ArgtypeString
+                     , argDesc = "the file to interpret"
+                     }
+                   ]
+
+runInterpreter :: InterpreterSettings -> IO ()
+runInterpreter settings = case file settings of
+    Just file -> runFile file
+    Nothing -> repl
 
 repl :: IO ()
 repl = putStr "> " >> hFlush stdout >> getLine >>= run >> repl
