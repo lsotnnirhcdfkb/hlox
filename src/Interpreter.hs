76d079bee66dfec0c45c7316717c06137e376e81
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-02-09 23:03:24 -0500

    separate arg parsing from running

---
+++ src/Interpreter.hs
@@ -0,0 +1,61 @@
+module Interpreter
+    ( interpret
+    , InterpreterSettings(..)
+    , ErrorFormat(..)
+    , Backend(..)
+    ) where
+
+import System.IO
+
+import Frontend.Scan
+import Frontend.Parse
+import Frontend.Diagnostic
+import qualified Treewalk.Interpret
+import Runtime.Value
+
+data InterpreterSettings = InterpreterSettings
+                           { errorFormat :: ErrorFormat
+                           , backend :: Backend
+                           , file :: Maybe String
+                           }
+
+data ErrorFormat = DefaultErrorFormat | OriginalErrorFormat
+                 deriving Show
+data Backend = Treewalk | StackBC | RegisterBC
+             deriving Show
+
+
+interpret :: InterpreterSettings -> IO ()
+interpret settings = case file settings of
+    Just file -> runFile settings file
+    Nothing -> repl settings
+
+repl :: InterpreterSettings -> IO ()
+repl settings = putStr "> " >> hFlush stdout >> getLine >>= run settings >> repl settings
+
+runFile :: InterpreterSettings -> String -> IO ()
+runFile settings fileName = readFile fileName >>= run settings
+
+run :: InterpreterSettings -> String -> IO ()
+run settings source =
+    let (scanned, scanErrors') = scan source
+        scanErrors = map toErr scanErrors'
+
+        (parsed, parseErrors') = parse scanned
+        parseErrors = map toErr parseErrors'
+
+        beforeRunErrs = scanErrors ++ parseErrors
+        reportBeforeRunErrs = mapM_ report beforeRunErrs
+        beforeRunSuccess = length beforeRunErrs == 0
+
+        treewalked = if beforeRunSuccess
+        then case parsed of
+                Just ast -> Just $ Treewalk.Interpret.interpret ast
+                _ -> error "parsed is Nothing but there are no before run errors"
+        else Nothing
+    in reportBeforeRunErrs >>
+    case treewalked of
+        Just (Right (Located _ res)) -> putStrLn $ stringifyValue res
+        Just (Left runErr) -> report $ toErr runErr
+        _ -> return ()
+
--- src/Main.hs
+++ src/Main.hs
@@ -3,44 +3,29 @@ module Main where
 import System.Environment
 import System.IO
 import System.Console.ParseArgs
-import Frontend.Scan
-import Frontend.Parse
-import Frontend.Diagnostic
-import Treewalk.Interpret
-import Runtime.Value
+import qualified Interpreter
 
-data InterpreterSettings = InterpreterSettings
-                           { errorFormat :: ErrorFormat
-                           , backend :: Backend
-                           , file :: Maybe String
-                           }
-
-data ErrorFormat = DefaultErrorFormat | OriginalErrorFormat
-                 deriving Show
-data Backend = Treewalk | StackBC | RegisterBC
-             deriving Show
-
-readInterpreterSettings :: Args String -> Either String InterpreterSettings
+readInterpreterSettings :: Args String -> Either String Interpreter.InterpreterSettings
 readInterpreterSettings args =
     let errorFormatStr = getRequiredArg args "errformat" :: String
         backendStr = getRequiredArg args "backend" :: String
         interpretingFile = getArg args "file" :: Maybe String
 
     in (case errorFormatStr of
-            "default" -> Right DefaultErrorFormat
+            "default" -> Right Interpreter.DefaultErrorFormat
             "original" -> Left "original error format is not implemented (yet)"
             _ -> Left $ "invalid error format: '" ++ errorFormatStr ++ "'"
         ) >>= \errorFormat ->
         (case backendStr of
-            "treewalk" -> Right Treewalk
+            "treewalk" -> Right Interpreter.Treewalk
             "stackbc" -> Left "stack bytecode interpreter is not implemented (yet)"
             "registerbc" -> Left "register bytecode interpreter is not implemented (yet)"
             _ -> Left $ "invalid backend: '" ++ backendStr ++ "'"
         ) >>= \backend ->
-        Right InterpreterSettings
-        { errorFormat = errorFormat
-        , backend = backend
-        , file = interpretingFile
+        Right Interpreter.InterpreterSettings
+        { Interpreter.errorFormat = errorFormat
+        , Interpreter.backend = backend
+        , Interpreter.file = interpretingFile
         }
 
 main :: IO ()
@@ -50,7 +35,7 @@ main =
     let parsedArgs = parseArgs argsControl argsSpec progName args
         settings = readInterpreterSettings parsedArgs
     in case settings of
-        Right set -> runInterpreter set
+        Right set -> Interpreter.interpret set
         Left err -> hPutStrLn stderr $ "hlox: " ++ err
     where
         argsControl = ArgsParseControl
@@ -79,38 +64,3 @@ main =
                      , argDesc = "the file to interpret"
                      }
                    ]
-
-runInterpreter :: InterpreterSettings -> IO ()
-runInterpreter settings = case file settings of
-    Just file -> runFile file
-    Nothing -> repl
-
-repl :: IO ()
-repl = putStr "> " >> hFlush stdout >> getLine >>= run >> repl
-
-runFile :: String -> IO ()
-runFile fileName = readFile fileName >>= run
-
-run :: String -> IO ()
-run source =
-    let (scanned, scanErrors') = scan source
-        scanErrors = map toErr scanErrors'
-
-        (parsed, parseErrors') = parse scanned
-        parseErrors = map toErr parseErrors'
-
-        beforeRunErrs = scanErrors ++ parseErrors
-        reportBeforeRunErrs = mapM_ report beforeRunErrs
-        beforeRunSuccess = length beforeRunErrs == 0
-
-        treewalked = if beforeRunSuccess
-        then case parsed of
-                Just ast -> Just $ interpret ast
-                _ -> error "parsed is Nothing but there are no before run errors"
-        else Nothing
-    in reportBeforeRunErrs >>
-    case treewalked of
-        Just (Right (Located _ res)) -> putStrLn $ stringifyValue res
-        Just (Left runErr) -> report $ toErr runErr
-        _ -> return ()
-
